30bee5fd4e92cf24e08b9b1cce9a3bd162196ac1
Hillan <hillanvieira@gmail.com>
2021-06-21 19:52:16 -0300

    Refactoring the code and implementing Notifications

--- app/src/main/java/br/com/hillan/gitissues/services/UpdateListWorker.kt
+++ app/src/main/java/br/com/hillan/gitissues/services/UpdateListWorker.kt
@@ -1,47 +1,65 @@
 package br.com.hillan.gitissues.services
 
 import android.app.NotificationManager
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.app.PendingIntent.getActivity
 import android.content.Context
 import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
+import android.content.SharedPreferences
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import br.com.hillan.gitissues.R
-import br.com.hillan.gitissues.models.Issue
 import br.com.hillan.gitissues.repository.IssueRepository
 import br.com.hillan.gitissues.ui.MainActivity
 import kotlinx.coroutines.GlobalScope
-import org.koin.standalone.KoinComponent
-import org.koin.standalone.inject
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import okhttp3.internal.wait
+import org.koin.standalone.KoinComponent
+import org.koin.standalone.inject
+
 
 class UpdateListWorker(context: Context, workerParams: WorkerParameters) :
-    Worker(context, workerParams), KoinComponent{
+    Worker(context, workerParams), KoinComponent {
 
     val repository: IssueRepository by inject()
+
+    var sharedpreferences: SharedPreferences =
+        applicationContext.getSharedPreferences("gitissues_preferences", Context.MODE_PRIVATE)
+
+    lateinit var oldLastIssue: String
+
+    lateinit var newLastIssue: String
 
     override fun doWork(): Result {
 
         repository.updateListToDb()
+        oldLastIssue = sharedpreferences.getString("lastIssueTitle", "no new issues")!!
 
         GlobalScope.launch {
-            repository.lastIssue.collect{it ->
-                sendNotification(it.title)}
+            delay(5000)
+
+            repository.lastIssue.collect { it ->
+                newLastIssue = it.title
+                sharedpreferences.edit().putString("lastIssueTitle", newLastIssue).commit()
+                if (oldLastIssue != newLastIssue) {
+                    sendNotification(it.title)
+                }
+            }
         }
 
-        Log.i("WORK TEST", "DONE")
-
+        Log.i("WORK_NOTIFICATION", "DONE")
         return Result.success()
     }
 
     private fun sendNotification(contentText: String) {
         val intent = Intent(applicationContext, MainActivity::class.java)
+        intent.flags = (Intent.FLAG_ACTIVITY_CLEAR_TOP
+                or Intent.FLAG_ACTIVITY_SINGLE_TOP)
 
         val notificationManager =
             applicationContext.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
@@ -53,13 +71,15 @@ class UpdateListWorker(context: Context, workerParams: WorkerParameters) :
             .setContentTitle("New Issue")
             .setContentText(contentText)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setContentIntent(pendingIntent).setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
 
-        with(NotificationManagerCompat.from(applicationContext)) {
-            // notificationId is a unique int for each notification that you must define
-            notify(1, notificationBuilder.build())
-        }
+        notificationManager.notify(1, notificationBuilder.build())
+
+//        with(NotificationManagerCompat.from(applicationContext)) {
+//            // notificationId is a unique int for each notification that you must define
+//            notify(1, notificationBuilder.build())
+//        }
     }
-
 
 }
