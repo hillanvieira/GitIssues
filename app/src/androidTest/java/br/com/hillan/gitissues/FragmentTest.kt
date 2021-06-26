ab61e8771aec91c856b55bcd98477ea4f30cae9a
Hillan <hillanvieira@gmail.com>
2021-06-25 23:15:49 -0300

    Fragments tests

--- app/src/androidTest/java/br/com/hillan/gitissues/FragmentTest.kt
+++ app/src/androidTest/java/br/com/hillan/gitissues/FragmentTest.kt
@@ -2,22 +2,23 @@ package br.com.hillan.gitissues
 
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
+
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import br.com.hillan.gitissues.ui.fragments.IssueListFragment
-import br.com.hillan.gitissues.ui.fragments.ViewIssueFragment
+
 import org.junit.Test
 import org.junit.runner.RunWith
+
+import br.com.hillan.gitissues.ui.fragments.ViewIssueFragment
 
 @RunWith(AndroidJUnit4::class)
 class FragmentTest {
 
     @Test
-    fun testEventFragment() {
-        // The "fragmentArgs" and "factory" arguments are optional.
+    fun deveAbrirOFragmentEVerSeOTituloDaIsseCorrespondeAoIDPassadoViaArgs() {
         val fragmentArgs = Bundle().apply {
             putLong("issueId", 930410497)
         }
@@ -26,5 +27,9 @@ class FragmentTest {
         onView(withId(R.id.view_issue_title)).check(matches(withText("[Portugal] Senior Android Developer")))
     }
 
+//    @Test
+//    fun testListIssueFragment(){
+//        val scenario = launchFragmentInContainer<ViewIssueFragment>()
+//    }
 
 }
